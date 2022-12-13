2cc367c58c634702f2d0064afdf0c6892c06656e
Aubrey Guishard <aubrey.guishard@gmail.com>
2022-12-13 17:05:52 -0500

    Was able to generate a while loop that loops until it reaches the variable favorite_number which equaled 7

--- problems_research.py
+++ problems_research.py
@@ -1,5 +1,9 @@
 import random
 
-print(random.randrange (4, 8))
+# print(random.randrange (4, 8))
 
-# favorite_number = 8
+favorite_number = 7
+
+while favorite_number >= 7:
+    favorite_number = random.randrange(4, 8)
+    print(favorite_number)
