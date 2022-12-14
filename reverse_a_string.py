fb0cbce42eba68defd48e1fd791fc5e097905a6f
Aubrey Guishard <aubrey.guishard@gmail.com>
2022-12-14 14:06:52 -0500

    I was able to get the Hello variable to print in reverse in the terminal following the the correct instructions from the slides

--- reverse_a_string.py
+++ reverse_a_string.py
@@ -1,3 +1,15 @@
 word = "Hello"
-reverse_word = word[4] + word[3] + word[2] + word[1] + word[0]
-print(reverse_word)
+
+reverse_word = " "
+
+for index in range(len(word) -1, -1, -1):
+   reverse_word += word[index]
+
+print(reverse_word)   
+
+# reverse_word = word[4] + word[3] + word[2] + word[1] + word[0]
+# print(reverse_word)
+
+# reverse_word = word[len(word) -1]
+
+# print(reverse_word)
